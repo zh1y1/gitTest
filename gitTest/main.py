f3093dbabf53a653e07dd7e1db5b2a00a563f691
LinZhiyi <466521333@qq.com>
2023-08-17 23:10:06 +0800

    third commit

--- gitTest/main.py
+++ gitTest/main.py
@@ -9,6 +9,9 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
     print("first commit")
 
+    print("second commit")
+    print("third commit")
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
