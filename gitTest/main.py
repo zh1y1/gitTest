78905113dee895c946435bd78661359f2af3ca29
LinZhiyi <466521333@qq.com>
2023-08-17 23:27:08 +0800

    forth commit

--- gitTest/main.py
+++ gitTest/main.py
@@ -9,8 +9,6 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
     print("first commit")
 
-    print("second commit")
-    print("third commit")
 
 
 # Press the green button in the gutter to run the script.
